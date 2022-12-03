acd11509167d65f872161902929e747041bbe5b9
lakinduchandula <lakindu.chandula@hotmail.com>
2022-12-03 11:55:46 +0530

    style: :lipstick: update UI home
    change fonts, icons, and replace the order

--- lib/frames/home.dart
+++ lib/frames/home.dart
@@ -1,10 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'image2text.dart';
 import 'realtime.dart';
 import 'realtimeAudio.dart';
 import 'text2sign.dart';
-import 'dart:io';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -20,13 +18,14 @@ class _HomeState extends State<Home> {
     Size size = MediaQuery.of(context).size;
     double containerHight = size.height + AppBar().preferredSize.height;
     double containerWidth = size.width * 0.8;
-    Color containerColor = Colors.lightBlueAccent;
+    Color containerColor = const Color(0xff324dfa);
 
     return Scaffold(
       backgroundColor: Colors.white,
       body: SingleChildScrollView(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Row(
               mainAxisAlignment: MainAxisAlignment.start,
@@ -52,20 +51,48 @@ class _HomeState extends State<Home> {
                   child: SingleChildScrollView(
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.start,
-                      children: const [
-                        SizedBox(
-                          height: 40,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(
+                          height: 60,
                         ),
-                        Text(
-                          "Hello,",
-                          style: TextStyle(color: Colors.white, fontSize: 25),
+                        Row(
+                          children: const [
+                            SizedBox(
+                              width: 42,
+                            ),
+                            Text(
+                              "Hello,",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontFamily: 'Montserrat',
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                            Text(
+                              " John",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 25,
+                                fontFamily: 'Montserrat',
+                                fontWeight: FontWeight.bold,
+                              ),
+                              textAlign: TextAlign.start,
+                            ),
+                          ],
                         ),
-                        SizedBox(
-                          height: 20,
+                        const SizedBox(
+                          height: 15,
                         ),
-                        Text(
-                          "What can i\ntranslate for\nyou today?",
-                          style: TextStyle(color: Colors.white, fontSize: 40),
+                        const Center(
+                          child: Text(
+                            "What can I\ntranslate for\nyou today?",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 40,
+                                fontFamily: 'Montserrat'),
+                          ),
                         )
                       ],
                     ),
@@ -92,7 +119,7 @@ class _HomeState extends State<Home> {
                 ),
               ],
             ),
-            Container(
+            SizedBox(
               width: size.width,
               child: Column(
                 children: [
@@ -100,44 +127,53 @@ class _HomeState extends State<Home> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              borderList[0] = true;
-                              borderList[1] =
-                              borderList[2] = borderList[3] = false;
-                            });
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => RealTimeTranslate()));
-                          },
-                          child: Container(
-                            width: 130,
-                            height: 130,
-                            decoration: boxDecoration(containerColor, 0),
-                            child: buttonContent(
-                                FontAwesomeIcons.camera, "Video Translate"),
-                          )),
+                        onTap: () {
+                          setState(
+                            () {
+                              borderList[3] = true;
+                              borderList[0] =
+                                  borderList[1] = borderList[2] = false;
+                            },
+                          );
+                          //sleep(Duration(milliseconds: 1000));
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const TextToSignLanguage(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 130,
+                          height: 130,
+                          decoration: boxDecoration(containerColor, 3),
+                          child: buttonContent('assets/icons/text.png', "Text"),
+                        ),
+                      ),
                       const SizedBox(width: 30),
                       GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              borderList[1] = true;
-                              borderList[0] =
-                              borderList[2] = borderList[3] = false;
-                            });
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => RealTimeAudioTranslate()));
-                          },
-                          child: Container(
-                            width: 130,
-                            height: 130,
-                            decoration: boxDecoration(containerColor, 1),
-                            child: buttonContent(
-                                FontAwesomeIcons.microphone, "Audio Translate"),
-                          )),
+                        onTap: () {
+                          setState(() {
+                            borderList[1] = true;
+                            borderList[0] =
+                                borderList[2] = borderList[3] = false;
+                          });
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) =>
+                                  const RealTimeAudioTranslate(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 130,
+                          height: 130,
+                          decoration: boxDecoration(containerColor, 1),
+                          child:
+                              buttonContent('assets/icons/audio.png', "Audio"),
+                        ),
+                      ),
                     ],
                   ),
                   const SizedBox(
@@ -147,45 +183,50 @@ class _HomeState extends State<Home> {
                     mainAxisAlignment: MainAxisAlignment.center,
                     children: [
                       GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              borderList[2] = true;
-                              borderList[0] =
-                              borderList[1] = borderList[3] = false;
-                            });
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => ImageToText()));
-                          },
-                          child: Container(
-                            width: 130,
-                            height: 130,
-                            decoration: boxDecoration(containerColor, 2),
-                            child: buttonContent(
-                                FontAwesomeIcons.image, "Image translate"),
-                          )),
+                        onTap: () {
+                          setState(() {
+                            borderList[0] = true;
+                            borderList[1] =
+                                borderList[2] = borderList[3] = false;
+                          });
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const RealTimeTranslate(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 130,
+                          height: 130,
+                          decoration: boxDecoration(containerColor, 0),
+                          child:
+                              buttonContent('assets/icons/camera.png', "Video"),
+                        ),
+                      ),
                       const SizedBox(width: 30),
                       GestureDetector(
-                          onTap: () {
-                            setState(() {
-                              borderList[3] = true;
-                              borderList[0] =
-                              borderList[1] = borderList[2] = false;
-                            });
-                            //sleep(Duration(milliseconds: 1000));
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => TextToSignLanguage()));
-                          },
-                          child: Container(
-                            width: 130,
-                            height: 130,
-                            decoration: boxDecoration(containerColor, 3),
-                            child: buttonContent(FontAwesomeIcons.solidKeyboard,
-                                "Text Translate"),
-                          )),
+                        onTap: () {
+                          setState(() {
+                            borderList[2] = true;
+                            borderList[0] =
+                                borderList[1] = borderList[3] = false;
+                          });
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const ImageToText(),
+                            ),
+                          );
+                        },
+                        child: Container(
+                          width: 130,
+                          height: 130,
+                          decoration: boxDecoration(containerColor, 2),
+                          child:
+                              buttonContent('assets/icons/image.png', "Image"),
+                        ),
+                      ),
                     ],
                   ),
                 ],
@@ -206,23 +247,31 @@ class _HomeState extends State<Home> {
         borderRadius: BorderRadius.circular(40),
         boxShadow: const [
           BoxShadow(offset: Offset(0, 2), blurRadius: 10, color: Colors.grey),
-          BoxShadow(
-              offset: Offset(-5, -5), blurRadius: 15, color: Colors.white)
+          BoxShadow(offset: Offset(-5, -5), blurRadius: 15, color: Colors.white)
         ]);
   }
 
-  Widget buttonContent(IconData icon, String text) {
+  Widget buttonContent(String iconPath, String text) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
-        Icon(
-          icon,
-          size: 60,
+        Container(
+          height: 80,
+          // color: Colors.deepPurple[400],
+          padding: const EdgeInsets.all(15),
+          child: Image.asset(
+            iconPath,
+            color: Colors.black,
+          ),
         ),
-       const SizedBox(
-          height: 10,
+        Text(
+          text,
+          style: const TextStyle(
+            color: Colors.black,
+            fontSize: 14,
+            fontFamily: 'Inter',
+          ),
         ),
-        Text(text),
       ],
     );
   }
