0a8bc90bd26ae3fd88620c41e3ae71b76493d2d1
lakinduchandula <lakindu.chandula@hotmail.com>
2022-12-18 18:33:08 +0530

    refactor: :art: refactor realtime audio translation dart file

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,19 @@ import 'package:flutter/services.dart';
 import 'package:sign_language_dictionary_app/frames/loading.dart';
 
 List<CameraDescription>? camera;
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  camera =await availableCameras();
-  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitDown,DeviceOrientation.portraitUp]);
+  camera = await availableCameras();
+  SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ],
+  );
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home:const LoadingPage(),
+      home: const LoadingPage(),
       theme: ThemeData(
         primaryColor: Colors.yellowAccent,
         accentColor: const Color(0xff324dfa),
